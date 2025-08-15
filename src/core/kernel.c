e1de1c24a85ec7171cc6b38f10a3a554831aca56
Ivan-Mathias <ivanmathiassilva@gmail.com>
2025-08-15 15:35:13 -0300

    Refactor project structure

--- src/core/kernel.c
+++ src/core/kernel.c
@@ -5,11 +5,11 @@
 void main() {
     uart_init();
     uart_puts("\n===== SimpleFS Bare-Metal no Raspberry Pi 3 =====\n");
-\
+
     fs_format();
     fs_mount();
     uart_puts("Sistema de arquivos formatado e montado.\n");
     uart_puts("Digite 'help' para ver os comandos.\n");
-\
+
     shell_start();
 }
